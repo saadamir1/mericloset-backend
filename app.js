e3a45dfde2aca8aa0abf5f9d106138a107c0fea9
Saad Amir <Saadamir070@gmail.com>
2024-09-30 00:53:21 +0500

    log api requests and establish mongodb connection

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+//middleware
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 require('dotenv/config');
 const api = process.env.API_URL;
 
 const PORT = process.env.PORT || 5170;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to MeriCloset Backend!');
+app.get(`${api}/products/`, (req, res) => {
+    const product = {
+        id: 1,
+        name: "hair dresser",
+        image: 'URL'
+    }
+    res.send(product);
 });
+
+app.post(`${api}/products/`, (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct)
+    res.send(newProduct);
+});
+
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() =>
+        console.log("database connection successful!"))
+    .catch((err) => console.log(err))
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
