f4175093d8dcc77609e4a7abfc060650fc62ff30
saadamir1 <saadamir070@gmail.com>
2025-06-14 17:27:19 +0500

    cors all origins allowed

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 //app.use(cors({ origin: corsOrigin }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
